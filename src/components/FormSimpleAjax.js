baadf523a2ef710af7e9563a7296f16e05e1cc52
Chris Eady <chris.eady1@gmail.com>
2019-07-06 20:44:16 -0500

    Add netlify recaptcha true

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -67,7 +67,7 @@ class Form extends React.Component {
           action={action}
           onSubmit={this.handleSubmit}
           data-netlify="true"
-          netlify-recaptcha=""
+          netlify-recaptcha="true"
         >
           {this.state.alert && (
             <div className="Form--Alert">{this.state.alert}</div>
